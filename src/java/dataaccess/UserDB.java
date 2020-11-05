89438dbde59f516a5fee094491082075f845bfd9
cocog <cocog@10.0.0.61>
2020-11-05 12:41:15 -0700

    Cleaned up Code

--- src/java/dataaccess/UserDB.java
+++ src/java/dataaccess/UserDB.java
@@ -28,17 +28,13 @@ public class UserDB {
         } finally {
             em.close();
         }
-
     }
 
     public User get(String email) throws Exception {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
         
-        
         try {
-            
             User user = em.find(User.class, email);
-            
             return user;
         } finally {
             em.close();
